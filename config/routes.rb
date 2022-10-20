7a3f10046f40ee2e02326811cd4297b01eca7922
EC2 Default User <ec2-user@ip-172-31-25-204.ap-northeast-1.compute.internal>
2022-10-20 06:55:37 +0000

    item_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,54 @@
 Rails.application.routes.draw do
+
+    namespace :admin do
+
+     get '/' => 'homes#top'
+
+     resources :items, only: [:index, :new, :create, :show, :edit, :update]
+
+     resources :genres, only: [:index, :create, :edit, :update]
+
+     resources :customers, only: [:index, :show, :edit, :update]
+
+     resources :orders, only: [:show, :update]
+
+     resources :order_details, only: [:update]
+
+    end
+
+
+
+    scope module: :public do
+
+     get root to: 'homes#top'
+     get 'about' => 'homes#about', as: 'about'
+
+     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
+     resources :orders, only: [:new, :create, :index, :show]
+     get 'orders/thanks' => 'orders#thanks'
+     post 'orders/confirm' => 'orders#confirm'
+
+     resources :cart_items, only: [:index, :update, :destroy, :create]
+     delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+
+     resource :customers, only: [:show, :update]
+     get 'customers/infomation/edit' => 'customers#edit'
+     get 'customers/unsubscribe' => 'customers#unsubscribe'
+     patch 'customers/withdrawal' => 'customers#withdrawal'
+
+     resources :items, only: [:index, :show]
+
+    end
+
+      devise_for :admin, skip: [:registrations, :passwords], controllers: {
+      sessions: "admin/sessions"
+     }
+
+      devise_for :customers, skip: [:passwords], controllers: {
+      registrations: "public/registrations",
+      sessions: 'public/sessions'
+      }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Admin.create!(
+  email: "tiyezhumai@gmail.com",
+  password: "chbtdzgj8259dhvcxkjg"
+  )
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
