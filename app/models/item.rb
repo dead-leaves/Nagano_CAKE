df51df317b3971c6f0cce6bac4324387d5972960
EC2 Default User <ec2-user@ip-172-31-25-204.ap-northeast-1.compute.internal>
2022-12-24 16:34:32 +0000

    admin_layout

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
-  has_many :cart_items 
+  has_many :cart_items
   has_many :customers, through: :cart_items
 
   has_many :order_details
@@ -13,12 +13,12 @@ class Item < ApplicationRecord
   def add_tax_price
     (price * 1.1).floor
   end
-  
-  
+
+
 
   def get_image(width, height)
     unless image.attached?
-     file_path = Rails.root.join('app/assets/images/no_image.jpg')
+     file_path = Rails.root.join('app/assets/images/C34455EA-2762-4966-93D1-F9254882B2A0_1_201_a.jpeg')
      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image.variant(resize_to_limit: [width, height]).processed
